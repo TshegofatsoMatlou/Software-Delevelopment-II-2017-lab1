8716ce11b9658d76adb1e7c1c62887d73ea17515
TshegofatsoMatlou <793138@students.wits.ac.za>
2017-08-15 19:06:32 +0400

    Update StopWatch.cpp

--- stopwatch/StopWatch.cpp
+++ stopwatch/StopWatch.cpp
@@ -1,5 +1,6 @@
 #include "StopWatch.h"
 #include <ctime>
+
 using namespace std;
 
 // returns the amount of time in seconds that has passed since the process (i.e. your program) started executing
@@ -9,12 +10,15 @@ double StopWatch::getProcessTime()
 	return static_cast<double>(time)/CLOCKS_PER_SEC;
 }
 //We did not add additional functions since the getProcessTime() function does all that is required except that it is not placed in a class
+
 void StopWatch::startTime(double initTime){
 	_startTime = initTime;
 }
+
 void StopWatch::endTime(double finalTime){
 	_endTime = finalTime;
 }
+
 double StopWatch::getRunTime(){
 	return (_endTime - _startTime);
 }
